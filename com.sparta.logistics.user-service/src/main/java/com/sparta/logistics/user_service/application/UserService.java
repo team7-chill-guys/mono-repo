9840c1d7931e790cc4ec5435178eba2d778590a9
Imnotcoderdude <topsolo7@naver.com>
2025-03-17 02:23:13 +0900

    ✨ feat[#9] : auth-service의 유저 생성과 user-service의 유저 생성 기능 연결 - auth-service의 유저 생성 기능 요청시 user-service의 유저 생성 기능과 연결. - BaseEntity 활용 방안 모색.

--- com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/application/UserService.java
+++ com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/application/UserService.java
@@ -1,8 +1,30 @@
 package com.sparta.logistics.user_service.application;
 
+import com.sparta.logistics.user_service.application.dto.request.UserSignupRequestDto;
+import com.sparta.logistics.user_service.application.dto.response.UserSignupResponseDto;
+import com.sparta.logistics.user_service.domain.entity.User;
+import com.sparta.logistics.user_service.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserSignupResponseDto createUser(UserSignupRequestDto requestDto) {
+        User user = User.builder()
+            .authId(requestDto.getAuthId())
+            .username(requestDto.getUsername())
+            .slackId(requestDto.getSlackId())
+            .build();
+
+        userRepository.save(user);
+
+        return UserSignupResponseDto.builder()
+            .username(user.getUsername())
+            .slackId(user.getSlackId())
+            .build();
+    }
 }
--- com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/infrastructure/repository/UserRepositoryImpl.java
+++ com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/infrastructure/repository/UserRepositoryImpl.java
@@ -5,7 +5,9 @@ import com.sparta.logistics.user_service.domain.repository.UserRepository;
 import com.sparta.logistics.user_service.infrastructure.jpa.UserJpaRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
@@ -15,6 +17,11 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public Optional<User> findById(Long id) {
         return userJpaRepository.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userJpaRepository.save(user);
     }
 
 }
