4ba7854990eaf5e33c94f1b747c592c305eb57e3
oh-min <ohyemin96@gmail.com>
2025-03-16 19:03:38 +0900

    ✨ feat [#16] : 허브 루트 Controller, Dto, Data 추가 - 허브 루트 도메인 Controller 파일 추가 - 허브 루트 도메인 Dto 파일 추가 - 테스트를 위한 임시 Data 파일 추가

---
+++ com.sparta.logistics.hub-service/src/main/java/com/sparta/logistics/hub_service/dto/UpdateHubRouteResponseDto.java
@@ -0,0 +1,16 @@
+package com.sparta.logistics.hub_service.dto;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UpdateHubRouteResponseDto {
+  private UUID startHubId;
+  private UUID endHubId;
+  private String startHubName;
+  private String endHubName;
+  private Long deliveryTime;
+  private Long deliveryDistance;
+}
