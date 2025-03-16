0f8a62158c212f8821c8886a4ab1b1406965b28a
Imnotcoderdude <topsolo7@naver.com>
2025-03-17 02:18:05 +0900

    ✨ feat[#7] : 유저 회원가입 기능 user-service의 user 생성 기능과 연결.

--- com.sparta.logistics.auth-service/src/main/java/com/sparta/logistics/auth_service/presentation/UserFeignClient.java
+++ com.sparta.logistics.auth-service/src/main/java/com/sparta/logistics/auth_service/presentation/UserFeignClient.java
@@ -1,10 +1,10 @@
 package com.sparta.logistics.auth_service.presentation;
 
-import com.sparta.logistics.auth_service.application.dto.request.UserSignupRequestDto;
+import com.sparta.logistics.auth_service.infrastructure.dto.request.UserSignupRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "http://localhost:19092/")
 public interface UserFeignClient {
 
     @PostMapping("/api/users/signup")
