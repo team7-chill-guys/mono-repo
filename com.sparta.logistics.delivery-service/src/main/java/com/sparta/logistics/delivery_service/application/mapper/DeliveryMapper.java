9d0afae1e3ca74c5e6995d7b8e7d0b5d5e7b21ef
Sohae Park <ipnsohe@naver.com>
2025-03-18 13:22:19 +0900

    🩹 fix[#1] : DeliveryMapper 수정
    - toDto 메서드에 createdAt 추가

--- com.sparta.logistics.delivery-service/src/main/java/com/sparta/logistics/delivery_service/application/mapper/DeliveryMapper.java
+++ com.sparta.logistics.delivery-service/src/main/java/com/sparta/logistics/delivery_service/application/mapper/DeliveryMapper.java
@@ -33,6 +33,7 @@ public class DeliveryMapper {
                 .departureHubId(delivery.getDepartureHubId())
                 .destinationHubId(delivery.getDestinationHubId())
                 .recipientCompany(delivery.getRecipientCompany())
+                .createdAt(delivery.getCreatedAt())
                 .build();
     }
 }
