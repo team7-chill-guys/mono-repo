51cf56dbd63aec2955f3dc14f80b66a15527bee3
Imnotcoderdude <topsolo7@naver.com>
2025-03-18 17:58:30 +0900

    ✨ feat[#9] : user 본인 조회 기능 구현. - 헤더에서 userId 값을 추출하여 본인 조회 구현.

--- com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/application/service/UserService.java
+++ com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/application/service/UserService.java
@@ -1,5 +1,6 @@
 package com.sparta.logistics.user_service.application.service;
 
+import com.sparta.logistics.user_service.application.dto.response.UserSearchMeResponseDto;
 import com.sparta.logistics.user_service.application.dto.response.UserSearchResponseDto;
 import com.sparta.logistics.user_service.domain.entity.User;
 import com.sparta.logistics.user_service.domain.repository.UserRepository;
@@ -16,6 +17,21 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    // 유저 검색 : 본인
+    public UserSearchMeResponseDto searchMeUser(String userIdHeader) {
+
+        Long userId = Long.parseLong(userIdHeader);
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("본인 정보를 찾을 수 없습니다. 헤더로 받은 userId : " + userIdHeader));
+        return UserSearchMeResponseDto.builder()
+            .id(user.getId())
+            .username(user.getUsername())
+            .slackId(user.getSlackId())
+            .role(user.getRole().toString())
+            .build();
+    }
+
+    // 유저 검색 : userId, username
     public UserSearchResponseDto searchUser(Long userId, String username) {
 
         User user;
--- com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/presentation/controller/UserController.java
+++ com.sparta.logistics.user-service/src/main/java/com/sparta/logistics/user_service/presentation/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +20,18 @@ public class UserController {
 
     private final UserService userService;
 
+    // 유저 검색 : 본인
     @GetMapping("/users/me")
-    public ResponseEntity<UserSearchMeResponseDto> getUserMe() {
-        UserSearchMeResponseDto responseDto = UserSearchMeResponseDto.builder()
-            .id(1L)
-            .username("testUser")
-            .slackId("testSlackId")
-            .role("MASTER")
-            .build();
-
+    public ResponseEntity<UserSearchMeResponseDto> getUserMe(@RequestHeader(value = "X-User-Id", required = false) String userIdHeader) {
+        if (userIdHeader == null) {
+            log.warn("header로 받은 정보에 X-User-Id가 존재하지 않습니다. X-User-Id : {}", userIdHeader);
+            return ResponseEntity.badRequest().build();
+        }
+        UserSearchMeResponseDto responseDto = userService.searchMeUser(userIdHeader);
         return ResponseEntity.ok(responseDto);
     }
 
+    // 유저 검색 : userId, username 기반. 동시 검색 X
     @GetMapping("/users/search")
     public ResponseEntity<UserSearchResponseDto> searchUser(@RequestParam(required = false) Long userId,
                                                             @RequestParam(required = false) String username
