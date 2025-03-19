cdeee052ef8e92073788e8465f8df8291f0f626d
Sohae Park <ipnsohe@naver.com>
2025-03-20 01:08:55 +0900

    ✨ feat [#55] : HubRouteClient 구현

---
+++ com.sparta.logistics.delivery-service/src/main/java/com/sparta/logistics/delivery_service/application/dto/response/HubRouteListResponseDto.java
@@ -0,0 +1,13 @@
+package com.sparta.logistics.delivery_service.application.dto.response;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class HubRouteListResponseDto {
+    private UUID startHubId;
+    private UUID endHubId;
+    private Integer deliveryTime;
+    private Double deliveryDistance;
+}
