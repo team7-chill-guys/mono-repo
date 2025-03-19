64f4b075004b3cfd3cc5b8dfe6f6feeafaa627dc
jcowwk <jcowwk@gmail.com>
2025-03-19 23:47:44 +0900

    ✨ feature[#60]: AI 서비스 생성

---
+++ com.sparta.logistics.ai-service/src/main/java/com/sparta/logistics/ai_service/infrastructure/dto/response/GeminiResponseDto.java
@@ -0,0 +1,58 @@
+package com.sparta.logistics.ai_service.infrastructure.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class GeminiResponseDto {
+    private List<Candidate> candidates;
+
+    public List<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public void setCandidates(List<Candidate> candidates) {
+        this.candidates = candidates;
+    }
+
+    public static class Candidate {
+        private Content content;
+
+        public Content getContent() {
+            return content;
+        }
+
+        public void setContent(Content content) {
+            this.content = content;
+        }
+    }
+
+    public static class Content {
+        private List<Part> parts;
+
+        public List<Part> getParts() {
+            return parts;
+        }
+
+        public void setParts(List<Part> parts) {
+            this.parts = parts;
+        }
+    }
+
+    public static class Part {
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+    }
+}
