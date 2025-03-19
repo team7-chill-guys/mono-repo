5a6138c7304d91abbb3f42f05ee0173039e804fa
jcowwk <jcowwk@gmail.com>
2025-03-19 16:35:34 +0900

    🩹 fix: AI 서비스 수정

---
+++ com.sparta.logistics.ai-service/src/main/java/com/sparta/logistics/ai_service/application/service/AiService.java
@@ -0,0 +1,33 @@
+package com.sparta.logistics.ai_service.application.service;
+
+import com.example.demo.infrastructure.client.GeminiClient;
+import com.example.demo.infrastructure.dto.request.GeminiRequestDto;
+import com.example.demo.infrastructure.dto.response.GeminiResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AiService {
+
+    private final GeminiClient geminiClient;
+
+    public AiService(GeminiClient geminiClient) {
+        this.geminiClient = geminiClient;
+    }
+
+    @Value("${googleai.api.key}")
+    private String GEMINI_API_KEY;
+
+    public String getCompletionText(String inputForAI) {
+        String request = inputForAI + "에 대한 배송 시간을 계산하여 정확한 배송 일자를 50자 이내로 작성해주세요.";
+        GeminiRequestDto requestDto = new GeminiRequestDto(request);
+
+        GeminiResponseDto responseDto = geminiClient.getCompletion(requestDto, GEMINI_API_KEY);
+
+        if (responseDto.getCandidates() != null && !responseDto.getCandidates().isEmpty()) {
+            String text = responseDto.getCandidates().get(0).getContent().getParts().get(0).getText();
+            return text;
+        }
+        return "답변을 받을 수 없습니다.";
+    }
+}
