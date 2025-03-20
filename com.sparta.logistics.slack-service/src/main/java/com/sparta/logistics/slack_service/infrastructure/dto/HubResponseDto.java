f8f24aec84360eb22bc9781bbf8cb0e0f692d900
jcowwk <jcowwk@gmail.com>
2025-03-20 15:56:48 +0900

    fix: 슬랙 허브 Client 수정

--- com.sparta.logistics.slack-service/src/main/java/com/sparta/logistics/slack_service/infrastructure/dto/HubResponseDto.java
+++ com.sparta.logistics.slack-service/src/main/java/com/sparta/logistics/slack_service/infrastructure/dto/HubResponseDto.java
@@ -2,12 +2,18 @@ package com.sparta.logistics.slack_service.infrastructure.dto;
 
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class HubResponseDto {
-    private String name;
+    private UUID id;
+    private Long userId;
+    private String hubName;
     private String address;
+    private Double latitude;
+    private Double longitude;
 }
