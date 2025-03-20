c11dba54df45550ed981825250f41866bd54a109
jcowwk <jcowwk@gmail.com>
2025-03-20 12:31:33 +0900

    ✨ feature: 슬랙 허브 Client 생성

--- com.sparta.logistics.slack-service/src/main/java/com/sparta/logistics/slack_service/infrastructure/client/DeliveryClient.java
+++ com.sparta.logistics.slack-service/src/main/java/com/sparta/logistics/slack_service/infrastructure/client/DeliveryClient.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
+
 @FeignClient(name = "delivery-api")
 public interface DeliveryClient {
     @GetMapping("/api/deliveries/{deliveryId}")
-    DeliveryResponseDto getDeliveryById(@PathVariable("deliveryId") Long deliveryId);
+    DeliveryResponseDto getDeliveryById(@PathVariable("deliveryId") UUID deliveryId);
 }
