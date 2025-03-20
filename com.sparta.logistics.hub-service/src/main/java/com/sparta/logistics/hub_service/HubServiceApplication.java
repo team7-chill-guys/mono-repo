b9ab64f8555ea768b6a58fc7680a4b6736b826ce
oh-min <ohyemin96@gmail.com>
2025-03-20 10:52:23 +0900

    feat[#44]: Eureka 설정 추가 및 Feign Client 활성화
    - Eureka 관련 dependencies 추가
    - @EnableFeignClients 어노테이션 추가하여 Feign Client 활성화

--- com.sparta.logistics.hub-service/src/main/java/com/sparta/logistics/hub_service/HubServiceApplication.java
+++ com.sparta.logistics.hub-service/src/main/java/com/sparta/logistics/hub_service/HubServiceApplication.java
@@ -2,10 +2,12 @@ package com.sparta.logistics.hub_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HubServiceApplication {
 
 	public static void main(String[] args) {
