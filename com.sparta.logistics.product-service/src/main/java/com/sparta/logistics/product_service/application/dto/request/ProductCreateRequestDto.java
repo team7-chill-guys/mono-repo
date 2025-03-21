d19167008ed2dbb6f5079f8dec38cd4c04193664
jcowwk <jcowwk@gmail.com>
2025-03-21 18:54:40 +0900

    fix: 통합 테스트 상품 수정

--- com.sparta.logistics.product-service/src/main/java/com/sparta/logistics/product_service/application/dto/request/ProductCreateRequestDto.java
+++ com.sparta.logistics.product-service/src/main/java/com/sparta/logistics/product_service/application/dto/request/ProductCreateRequestDto.java
@@ -1,17 +1,124 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.sparta.logistics.product_service.application.dto.request;
 
-import lombok.*;
-
 import java.util.UUID;
+import lombok.Generated;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class ProductCreateRequestDto {
     private UUID companyId;
     private UUID hubId;
     private String name;
     private Long stock;
+
+    @Generated
+    public static ProductCreateRequestDtoBuilder builder() {
+        return new ProductCreateRequestDtoBuilder();
+    }
+
+    @Generated
+    public UUID getCompanyId() {
+        return this.companyId;
+    }
+
+    @Generated
+    public UUID getHubId() {
+        return this.hubId;
+    }
+
+    @Generated
+    public String getName() {
+        return this.name;
+    }
+
+    @Generated
+    public Long getStock() {
+        return this.stock;
+    }
+
+    @Generated
+    public void setCompanyId(final UUID companyId) {
+        this.companyId = companyId;
+    }
+
+    @Generated
+    public void setHubId(final UUID hubId) {
+        this.hubId = hubId;
+    }
+
+    @Generated
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Generated
+    public void setStock(final Long stock) {
+        this.stock = stock;
+    }
+
+    @Generated
+    public ProductCreateRequestDto() {
+    }
+
+    @Generated
+    public ProductCreateRequestDto(final UUID companyId, final UUID hubId, final String name, final Long stock) {
+        this.companyId = companyId;
+        this.hubId = hubId;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    @Generated
+    public static class ProductCreateRequestDtoBuilder {
+        @Generated
+        private UUID companyId;
+        @Generated
+        private UUID hubId;
+        @Generated
+        private String name;
+        @Generated
+        private Long stock;
+
+        @Generated
+        ProductCreateRequestDtoBuilder() {
+        }
+
+        @Generated
+        public ProductCreateRequestDtoBuilder companyId(final UUID companyId) {
+            this.companyId = companyId;
+            return this;
+        }
+
+        @Generated
+        public ProductCreateRequestDtoBuilder hubId(final UUID hubId) {
+            this.hubId = hubId;
+            return this;
+        }
+
+        @Generated
+        public ProductCreateRequestDtoBuilder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        @Generated
+        public ProductCreateRequestDtoBuilder stock(final Long stock) {
+            this.stock = stock;
+            return this;
+        }
+
+        @Generated
+        public ProductCreateRequestDto build() {
+            return new ProductCreateRequestDto(this.companyId, this.hubId, this.name, this.stock);
+        }
+
+        @Generated
+        public String toString() {
+            String var10000 = String.valueOf(this.companyId);
+            return "ProductCreateRequestDto.ProductCreateRequestDtoBuilder(companyId=" + var10000 + ", hubId=" + String.valueOf(this.hubId) + ", name=" + this.name + ", stock=" + this.stock + ")";
+        }
+    }
 }
---
+++ com.sparta.logistics.product-service/src/main/java/com/sparta/logistics/product_service/infrastructure/client/HubClient.java
@@ -0,0 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.sparta.logistics.product_service.infrastructure.client;
+
+import com.sparta.logistics.product_service.infrastructure.client.dto.response.HubDetailResponseDto;
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "hub-service"
+)
+public interface HubClient {
+    @GetMapping({"/api/hubs/{hubId}"})
+    HubDetailResponseDto getHub(@PathVariable("hubId") UUID id);
+}
