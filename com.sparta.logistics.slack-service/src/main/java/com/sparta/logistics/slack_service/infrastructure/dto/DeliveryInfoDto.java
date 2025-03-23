17d3f62397f0c511d163807aba2249f52db4cf36
jcowwk <jcowwk@gmail.com>
2025-03-23 21:31:36 +0900

    fix: 슬랙 consumer kafka 설정 수정

---
+++ com.sparta.logistics.slack-service/src/main/java/com/sparta/logistics/slack_service/infrastructure/dto/DeliveryInfoDto.java
@@ -0,0 +1,20 @@
+package com.sparta.logistics.slack_service.infrastructure.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeliveryInfoDto {
+    UUID departureHubId;
+    UUID destinationHubId;
+    Long deliveryManagerId;
+    UUID orderId;
+    String slackId;
+}
