8124539c491a4fb8251f5b1b7c3c0cf618cc40dd
Sohae Park <ipnsohe@naver.com>
2025-03-23 16:28:07 +0900

    🩹 fix : 슬랙서비스로 보낼 메세지에 주소 추가

--- com.sparta.logistics.delivery-service/src/main/java/com/sparta/logistics/delivery_service/application/mapper/DeliveryInfoMapper.java
+++ com.sparta.logistics.delivery-service/src/main/java/com/sparta/logistics/delivery_service/application/mapper/DeliveryInfoMapper.java
@@ -5,13 +5,14 @@ import com.sparta.logistics.delivery_service.domain.model.Delivery;
 
 public class DeliveryInfoMapper {
 
-    public static DeliveryInfoDto toDto(Delivery delivery, String slackId) {
+    public static DeliveryInfoDto toDto(Delivery delivery, String slackId, String address) {
         return DeliveryInfoDto.builder()
                 .departureHubId(delivery.getDepartureHubId())
                 .destinationHubId(delivery.getDestinationHubId())
                 .deliveryManagerId(delivery.getDeliveryManagerId())
                 .orderId(delivery.getOrderId())
                 .slackId(slackId)
+                .address(address)
                 .build();
     }
 }
