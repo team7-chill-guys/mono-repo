62185ecf18dfc0b63c9b66ed295dfc1cfcee4cdc
Jjini <sojinnuna@gmail.com>
2025-03-24 11:19:11 +0900

    [feature] hub 존재 여부 확인 추가

--- com.sparta.logistics.company-service/src/main/java/com/sparta/logistics/company_service/application/service/CompanyService.java
+++ com.sparta.logistics.company-service/src/main/java/com/sparta/logistics/company_service/application/service/CompanyService.java
@@ -5,10 +5,11 @@ import com.sparta.logistics.company_service.application.dto.request.CompanyUpdat
 import com.sparta.logistics.company_service.application.dto.response.CompanyDetailResponseDto;
 import com.sparta.logistics.company_service.domain.Company;
 import com.sparta.logistics.company_service.domain.HubId;
+import com.sparta.logistics.company_service.infrastructure.client.HubClient;
 import com.sparta.logistics.company_service.repository.CompanyRepository;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ import java.util.UUID;
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
+    private final HubClient hubClient;
 
     // [배송에서 아이디 가져오기]
     public UUID getHubIdByCompanyId(UUID companyId) {
@@ -40,6 +42,16 @@ public class CompanyService {
     @Transactional
     public CompanyDetailResponseDto createCompany(CompanyCreateRequestDto requestDto, String userIdHeader) {
         Long userId = Long.parseLong(userIdHeader);
+
+        // 허브 존재 여부 검증
+        try {
+            hubClient.checkHubExists(requestDto.getHubId());
+        } catch (FeignException e) {
+            if (e.status() == 404) {
+                throw new RuntimeException("허브가 존재하지 않습니다.");
+            }
+            throw new RuntimeException("허브 서비스 호출 중 오류가 발생했습니다.");
+        }
 
         Company company = Company.create(requestDto, new HubId(requestDto.getHubId()), userId);
         companyRepository.save(company);
