611b7d907a21c917110ab8a54e62e02529689c40
oh-min <ohyemin96@gmail.com>
2025-03-25 14:03:57 +0900

    refactor[#182]: HubRoute 유효성 검사 추가

--- com.sparta.logistics.hub-service/src/main/java/com/sparta/logistics/hub_service/hubroute/application/dto/request/HubRouteCreateRequestDto.java
+++ com.sparta.logistics.hub-service/src/main/java/com/sparta/logistics/hub_service/hubroute/application/dto/request/HubRouteCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.logistics.hub_service.hubroute.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class HubRouteCreateRequestDto {
 
+  @NotNull(message = "출발 허브 ID는 필수입니다.")
   private UUID startHubId;
+
+  @NotNull(message = "도착 허브 ID는 필수입니다.")
   private UUID endHubId;
 }
